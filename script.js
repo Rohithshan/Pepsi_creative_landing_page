b9916347e913509a684c1c3ae423aef184b1cab2
Rohithshan <53473402+Rohithshan@users.noreply.github.com>
2021-08-18 21:41:09 +0530

    Add files via upload
    Need to update one thing, do it as soon as possible....

---
+++ script.js
@@ -0,0 +1,11 @@
+function imgSlider(anything) {
+    document.querySelecter('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
+
+// document.querySelector('.pepsi').src = "Media/pepsi003.png";
+// document.querySelector('.sec').style.background = "#1e1e1e";
